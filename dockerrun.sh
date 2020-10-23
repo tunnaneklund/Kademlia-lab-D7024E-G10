85488592797d23f005a4730c61e771aca63120f6
MartinBrathen <martin.viklund.brathen@gmail.com>
2020-10-23 16:02:23 +0200

    bash script

---
+++ dockerrun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker run -d -p 8080:8080 --name "cont0" --network mynetwork kademlia /app/main "cont0"
+for i in {1..50}
+    do
+        let PORT=8080+$i
+        docker run -d -p ${port}:8080 --name "cont$i" --network mynetwork kademlia /app/main "cont$i" "cont0"
+    done
