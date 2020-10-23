c850fd08078eeb621bea9bc09fabb45089d3ed3a
MartinBrathen <martin.viklund.brathen@gmail.com>
2020-10-23 16:03:43 +0200

    typo

--- dockerrun.sh
+++ dockerrun.sh
@@ -3,5 +3,5 @@ docker run -d -p 8080:8080 --name "cont0" --network mynetwork kademlia /app/main
 for i in {1..50}
     do
         let PORT=8080+$i
-        docker run -d -p ${port}:8080 --name "cont$i" --network mynetwork kademlia /app/main "cont$i" "cont0"
+        docker run -d -p ${PORT}:8080 --name "cont$i" --network mynetwork kademlia /app/main "cont$i" "cont0"
     done
